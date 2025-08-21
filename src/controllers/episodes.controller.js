98c8d5b69059d8bce2c595922f78fa373fc5fd27
titan <flameplayz937@gmail.com>
2025-08-21 11:51:33 +0100

    Update episodes.controller.js

--- src/controllers/episodes.controller.js
+++ src/controllers/episodes.controller.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
-import { validationError } from '../utils/errors';
-import config from '../config/config';
-import { extractEpisodes } from '../extractor/extractEpisodes';
+import { validationError } from '../utils/errors.js';
+import config from '../config/config.js';
+import { extractEpisodes } from '../extractor/extractEpisodes.js';
+import { fetchVidnestHindiStreams } from '../extractor/vidnest.js'; // new import
 
+/**
+ * Episodes Controller
+ * Fetches all episode info (sub, dub, and Hindi dubbed) for an anime.
+ */
 const episodesController = async (c) => {
   const id = c.req.param('id');
 
@@ -13,6 +18,7 @@ const episodesController = async (c) => {
   const ajaxUrl = `/ajax/v2/episode/list/${idNum}`;
 
   try {
+    // 1️⃣ Fetch sub/dub episodes from the site
     const { data } = await axios.get(config.baseurl + ajaxUrl, {
       headers: {
         Referer: Referer,
@@ -20,12 +26,28 @@ const episodesController = async (c) => {
       },
     });
 
-    const response = extractEpisodes(data.html);
-    return response;
-  } catch (err) {
-    console.log(err.message);
+    const episodes = extractEpisodes(data.html);
 
-    throw new validationError('make sure the id is correct', { validIdEX: 'one-piece-100' });
+    // 2️⃣ Fetch Hindi dubbed streams from Vidnest
+    const hindiStreamsPromises = episodes.map((ep) =>
+      fetchVidnestHindiStreams(idNum, ep.episodeNumber)
+    );
+
+    const hindiStreamsResults = await Promise.all(hindiStreamsPromises);
+
+    // 3️⃣ Attach Hindi streams to each episode
+    const enrichedEpisodes = episodes.map((ep, index) => ({
+      ...ep,
+      hindiStreams: hindiStreamsResults[index] || [],
+    }));
+
+    return enrichedEpisodes;
+  } catch (err) {
+    console.error(err.message);
+
+    throw new validationError('Make sure the id is correct', {
+      validIdEX: 'one-piece-100',
+    });
   }
 };
 
